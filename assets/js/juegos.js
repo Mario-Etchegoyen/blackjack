917bc19303547e965845f7329eba00cd8bb525c3
Aprendiz Eterno <mario14012003@yahoo.com.ar>
2024-03-15 14:38:14 -0300

    tamano cartas con max y min

--- assets/js/juegos.js
+++ assets/js/juegos.js
@@ -1,3 +1,51 @@
 
 
-console.log("mario")
+/*
+
+2c trebol
+2h corazones
+2s espadas pique
+2d diamante
+*/
+
+
+
+
+let deck = [];
+const tipos = ["C", "H", "S", "D"]
+const especiales = ["A", "J", "Q", "K"]
+
+const crearDeck = () => {
+  // ---------------------- criterio curso
+  // for (let i = 2; i <= 10; i++) {
+  //   for (let tipo of tipos) {
+  //     deck.push(i + tipo)
+  //   }
+  // }
+
+  // for (let especial of especiales) {
+  //   for(let tipo of tipos){
+  //     deck.push(especial + tipo)
+  //   }
+
+  // }
+
+
+  // ---------------- criterio propio
+  for (let tipo of tipos) {
+    for (let i = 2; i <= 10; i++) {
+      deck.push(i + tipo)
+    }
+    for (let especial of especiales) {
+      deck.push(especial + tipo)
+    }
+  }
+
+  console.log("mazo ordenado:", deck)
+  deck = _.shuffle(deck)
+  // console.log("mazo mezclado:",deck)
+  return deck
+}
+
+console.log(`mazo mezclado: ${crearDeck(deck)}. Total ${deck.length} cartas`)
+
