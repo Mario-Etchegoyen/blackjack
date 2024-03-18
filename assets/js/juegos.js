0a809867e5bd4927e097e8d69e94da653b95ba98
Aprendiz Eterno <mario14012003@yahoo.com.ar>
2024-03-17 23:35:42 -0300

    trabajando con btns selectores

--- assets/js/juegos.js
+++ assets/js/juegos.js
@@ -14,26 +14,23 @@
 let deck = [];
 const tipos = ["C", "H", "S", "D"]
 const especiales = ["A", "J", "Q", "K"]
+let ptsAcum=0,
+    ptsComputadora=0;
+
+
+// referencias HTML
+
+const btnNuevo=document.querySelector("#btnNuevo"),
+      btnPedir = document.querySelector("#btnPedir"),
+      btnDetener = document.querySelector("#btnDetener");
+
+const smalls = document.querySelectorAll("small");
+
 
 
 // ----------------------------------------- crear y mezclar mazo de cartas
 const crearDeck = () => {
-  // ---------------------- criterio curso
-  // for (let i = 2; i <= 10; i++) {
-  //   for (let tipo of tipos) {
-  //     deck.push(i + tipo)
-  //   }
-  // }
-
-  // for (let especial of especiales) {
-  //   for(let tipo of tipos){
-  //     deck.push(especial + tipo)
-  //   }
-
-  // }
-
-
-  // ---------------- criterio propio
+  // ---------------- criterio propio 3 for
   for (let tipo of tipos) {
     for (let i = 2; i <= 10; i++) {
       deck.push(i + tipo)
@@ -43,49 +40,67 @@ const crearDeck = () => {
     }
   }
 
-  console.log("mazo ordenado:", deck)
-  deck = _.shuffle(deck)
-  // console.log("mazo mezclado:",deck)
+  deck = _.shuffle(deck) // mezcla las cartas  
   return deck
 }
 
-console.log(`mazo mezclado: ${crearDeck(deck)}. Total ${deck.length} cartas`)
+// console.log(`mazo mezclado: ${crearDeck(deck)}. Total ${deck.length} cartas`)
 
 // ---------------------------------------- para tomar una carta
 
 const pedirCarta = () => {
   if (deck.length > 0) {
     cartaPedida = deck.pop()
-    // console.log("mazo:", deck, "carta pedida:", cartaPedida)
     return cartaPedida
   } else {
     console.log("El mazo esta vacio")
   }
 
 }
-// console.log((deck = [3, 4])) // de ejemplo para ver resultados de la funcion pedir Carta
-
-// console.log(pedirCarta())
-// console.log(pedirCarta())
-// console.log(pedirCarta())
-
-// pedirCarta()
-// pedirCarta()
-// pedirCarta()
 
 // ---------------------------- darle un valor a la carta
-const valorCarta = (carta => {
-  let puntos=0;
+const valorCarta = (carta) => {
+  let puntos = 0;
   const valor = carta.substring(0, carta.length - 1) // carta viene con "10d" y lo convierte en un arreglo para guardar hasta el indice indicado
-  console.log( ((isNaN(valor)) ?
+  puntos = (((isNaN(valor)) ?
     (valor === "A") ? puntos = puntos + 11 : puntos = puntos + 10
     : puntos = parseInt(valor, 10) + puntos));
-  console.log("carta:",carta,"puntaje:", puntos)
+  return puntos
+}
+
+const sumaPuntos = (puntaje = 0) => {
+  ptsAcum = ptsAcum + puntaje;
+  console.log(`Carta: ${cartaPedida}, puntaje: ${puntaje}, Punt Acum: ${ptsAcum}`) // devuelve string
+  return ptsAcum
+}
+
+const verificacion = (ptsAcum) => {
+  ptsAcum <= 21 ? console.log("pedis Otra?") : smalls[0].innerText ="PERDISTE!"
+
+}
 
 
+// verificacion(sumaPuntos(valorCarta(pedirCarta())))
+// verificacion(sumaPuntos(valorCarta(pedirCarta())))
+// verificacion(sumaPuntos(valorCarta(pedirCarta())))
+// verificacion(sumaPuntos(valorCarta(pedirCarta())))
+// verificacion(sumaPuntos(valorCarta(pedirCarta())))
+
+// ----------- eventos ---------------------------
+btnPedir.addEventListener("click", () => {
+  const carta=pedirCarta();
+  // console.log(carta);
+  const puntajeCarta=valorCarta(carta);
+  // console.log(puntajeCarta);
+  const ptsAcum=sumaPuntos(puntajeCarta);
+  smalls[0].innerText=ptsAcum;
+  verificacion(ptsAcum);
+} )
+
+btnNuevo.addEventListener("click", () => {
+  deck=[];
+  smalls[0].innerText =0;
+  ptsAcum=0;
+  console.log(crearDeck())
 })
 
-valorCarta(pedirCarta())
-valorCarta(pedirCarta())
-valorCarta(pedirCarta())
-valorCarta(pedirCarta())
